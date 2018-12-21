65842af77ad9d635bb1348b985d2f72f44ca2d8f
ahribori <ahribori@gmail.com>
2018-12-22 02:28:48 +0900

    fix client example

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
  ============================================*/
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const path = require('path');
 const createServer = require('./socket.io');
 const config = require('./conf');
@@ -17,8 +18,22 @@ const port = process.argv[2] || process.env.PORT || config.port;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Resolve client example
+app.get('/', (req, res) => {
+  fs.readFile(path.resolve('client_example', 'index.html'),
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+      res.writeHead(200);
+      data = data.toString().replace(/<%=host%>/gi, req.headers.host);
+      res.end(data);
+    });
+});
+
 // set public path
-app.use('/', express.static(path.resolve('client_example')));
+app.use('/', express.static(path.resolve('public')));
 
 /* handle error */
 app.use(function(err, req, res, next) {
