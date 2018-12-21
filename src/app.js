1ec7ae356919098544bcac33f65ecbed17a4282b
ahribori <ahribori@gmail.com>
2018-12-22 00:20:42 +0900

    update packages

--- src/app.js
+++ src/app.js
@@ -84,8 +84,8 @@ chatroom1.on('connection', (socket) => {
     // socket.leave('room1', () => {}); // 'room1' room 퇴장
     socket.on('message', (message) => {
         console.log('/chatroom1 -> ' + message);
-        // chatroom1.to('room1').emit('message', message); // 'chat1' namespace의 'room1' room에 boardcast
-        // socket.broadcast.to('room1').emit('message', message); // 'chat1' namespace의 'room1' room에 boardcast (위 방법과의 차이점은 모르겠다)
+        // chatroom1.to('room1').emit('message', message); // 'chat1' namespace의 'room1' room에 boardcast (모든 클라이언트에게 메세지를 보냄)
+        socket.broadcast.to('room1').emit('message', message); // 'chat1' namespace의 'room1' room에 boardcast (메세지를 보낸 클라이언트를 제외한 나머지에게 broadcast)
     });
 });
 
